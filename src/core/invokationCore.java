4f0bec4a21f47bb799a42f756269d2b015601650
Sharaf <mtharwat551@outlook.com>
2019-04-07 02:20:38 +0200

    Finalizing the invokationCore.

--- src/core/invokationCore.java
+++ src/core/invokationCore.java
@@ -1,14 +1,34 @@
 package core;
 
+import helpers.configurationHelper;
 import org.openqa.selenium.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Properties;
 
 public class invokationCore{
-    public static void invokeAppium(){
+    //A generic function to be configured for iOS and Android
+    public static WebDriver invokeAppium() throws IOException {
+        WebDriver appium;
 
+        //Calling configuration manager to return an object of the configuration file.
+        Properties properties = configurationHelper.getConfig();
+
+        //Desired Capabilities definitions from config file.
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("deviceName", properties.getProperty("deviceName"));
+        desiredCapabilities.setCapability("platformName", properties.getProperty("platformName"));
+        desiredCapabilities.setCapability("appiumDriverUrl", properties.getProperty("appiumDriverUrl"));
+        desiredCapabilities.setCapability("platformVersion", properties.getProperty("platformVersion"));
+        desiredCapabilities.setCapability("appPackage", properties.getProperty("appPackage"));
+        desiredCapabilities.setCapability("appWaitActivity", properties.getProperty("appWaitActivity"));
+        desiredCapabilities.setCapability("app", properties.getProperty("app"));
+
+        //Connecting the desired capabilities with the appium server.
+        appium = new RemoteWebDriver(new URL(properties.getProperty("appiumDriverUrl")), desiredCapabilities);
+        return appium;
     }
 }
